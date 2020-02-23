e666ee5467d080f2413ecbc9e8a80ad30fdb25d3
Aung T Tant <aung.top.touch7.gusto@gmail.com>
2020-02-23 12:13:53 +0630

    variable.py & list.py

---
+++ Variable.py
@@ -0,0 +1,89 @@
+int - integer - 1, 2, 3, ---> 308930 etc
+float - 	- 1.0,5.9, 6.0, 63.23, etc
+string - 	- 'Hello', "World", "A","B",
+
+a = 1
+a (variable) = (assign) 1 (value)
+
+width = 20
+height = 5 * 9
+vol = width * height
+vol
+#900
+
+sale = 1500
+tax = 5 / 100
+total_tax = sale * tax
+total_tax
+#75.0
+total_price = sale + total_tax
+total_price
+#1575.0
+round(total_price)
+
+print('spam eggs')
+print('don\'t')
+print("doesn't")
+print("Yes")
+print("\"Yes,\"they said.")
+print('"Isn\'t,"they said')
+
+print('"Isn\'t, they said"')
+
+# \n = newLine
+
+s = 'First Line. \nSecond Line.'
+print(s)
+
+print("""\
+Usage: thingy 
+		-a
+		-b
+		-c
+""")
+
+"""...""" or '''...'''
+
+3 * 'A'
+2 * 'BC' + 'DE'
+10 * 'GH' + '3'
+
+* or %
+
+5 * 3 + 2 % 3
+
+5 * 3 + 3 % 4
+
+6 * 9 - 4 * 5 % 2
+
+7 * 5 - 6 * 3 % 3
+
+6 * 5 - 7 * 4 / 4 % 2
+
+
+* / % + - (operator)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
